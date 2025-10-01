edb2e5359a601d799d6b1c795849b353f1917b02
AmoKiswaya <amosonganga254@gmail.com>
2025-10-01 10:04:23 +0300

    Enable signup function view to create a profile object for the user after the user creates an account

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth 
 from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 def index(request):
@@ -29,6 +30,17 @@ def signup(request):
                     password=password
                 )
                 user.save() 
+
+                # log user in and redirect to settings page
+
+                # create a Profile object for the new user
+                user_model = User.objects.get(username=username) 
+                new_profile = Profile.objects.create(
+                    user=user_model,
+                    id_user=user_model.id
+                ) 
+                new_profile.save()
+                return redirect('signup') 
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
